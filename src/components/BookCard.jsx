72ee3ced7bf6b78477c0af79a2bdb55ae182dbd8
monikamittal-1728 <monikagupta1728@gmail.com>
2026-05-29 13:24:40 +0530

    feat: add colorful fallback placeholder for missing cover images

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -26,7 +26,7 @@ const BookCard = ({ book }) => {
           <img
             src={
               book.image ||
-              "https://images.unsplash.com/photo-1532012197267-da84d127e765?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              "/no_cover_image.png"
             }
             alt={book.title}
             className="
